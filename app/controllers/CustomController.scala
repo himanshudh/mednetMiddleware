3d8e2e60eab77bb09c28987e79c3f5398b6bf8a7
himanshudh <himanshu.dhoke@gmail.com>
2016-08-21 14:44:04 +0530

    Change for remote actor

--- app/controllers/CustomController.scala
+++ app/controllers/CustomController.scala
@@ -4,12 +4,38 @@ package controllers
  * Created by himandhk on 04/08/2016.
  */
 
+import play.api.libs.concurrent.Akka
 import play.api.mvc.{Action, Controller}
-
+import akka.actor._
+import play.api.Play.current
 object CustomController extends Controller{
 
     def test = Action {
-      Ok("TEST")
-    }
 
+    //  val remote = Akka.system.actorOf("akka.tcp://HelloSystem@127.0.0.1:5150/user/superactor")
+      val myActor = Akka.system.actorOf(Props[LocalActor], name = "myactor")
+      myActor ! "START"
+      Ok("TEST")
+      }
+
+
+}
+
+
+class LocalActor extends Actor {
+
+  // create the remote actor (Akka 2.1 syntax)
+  val remote = context.actorFor("akka.tcp://HelloSystem@127.0.0.1:5150/user/superactor")
+  var counter = 0
+
+  def receive = {
+    case "START" =>
+      remote ! "super"
+    case msg: String =>
+      println(s"LocalActor received message: '$msg'")
+      if (counter < 5) {
+        sender ! "Hello back to you"
+        counter += 1
+      }
+  }
 }
